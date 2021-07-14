f738ed4a636654fc26a8782c0095f43e1d6f12ea
tphuc <nguoigiaumat100@gmail.com>
2021-07-14 16:02:48 +0700

    fix cmt - change Match to Matched in MatchResponse types

--- internal/app/api/types/match.go
+++ internal/app/api/types/match.go
@@ -24,7 +24,7 @@ type MatchResponse struct {
 	ID           bson.ObjectId    `json:"_id" bson:"_id,omitempty"`
 	UserID       bson.ObjectId    `json:"user_id" bson:"user_id"`
 	TargetUserID bson.ObjectId    `json:"target_user_id" bson:"target_user_id"`
-	Match        bool             `json:"matched" bson:"matched"`
+	Matched      bool             `json:"matched" bson:"matched"`
 	CreateAt     time.Time        `json:"created_at" bson:"created_at"`
 	TargetUser   []UserResGetInfo `json:"target_user" bson:"target_user"`
 }
